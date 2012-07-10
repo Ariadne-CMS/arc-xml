e5d76d15de45668d5918a9f2bd88e6e5dae9b847
Auke van Slooten <auke@poefke.nl>
2012-07-10 16:47:12 +0200

    added SimpleTest unit tests fixed a number of bugs in xml - failed tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+	require_once( __DIR__.'/../src/autoload.php');
+//	require_once( __DIR__.'/vendor/autoload.php');	
+
+	require_once( __DIR__.'/vendor/lastcraft/simpletest/autorun.php');
+	require_once( __DIR__.'/vendor/lastcraft/simpletest/compatibility.php');
+	require_once( __DIR__.'/vendor/lastcraft/simpletest/browser.php');
+	require_once( __DIR__.'/vendor/lastcraft/simpletest/web_tester.php');
+	require_once( __DIR__.'/vendor/lastcraft/simpletest/unit_tester.php');
