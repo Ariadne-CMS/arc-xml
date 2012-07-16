3548c07e57fb96674d2866638a4cb524cc6e88cf
Robbert Müller <muller@muze.nl>
2012-07-16 15:21:29 +0200

    Removed debug output

--- tests/testall.php
+++ tests/testall.php
@@ -3,7 +3,6 @@
 	
 	$files = scandir(__DIR__ . '/arc/');
 	foreach ( $files as $file ) {
-		print $file."\n";
 		if( is_file(__DIR__ . '/arc/'.$file) && basename( __DIR__ . '/arc/'. $file ,'.php') != $file ) {
 			include(__DIR__ . '/arc/'.$file);
 		}
