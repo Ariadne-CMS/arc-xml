c2dd57ecf028bb29f825b04bbfc6f36d464a3361
Robbert Müller <muller@muze.nl>
2012-07-21 12:25:57 +0200

    Adding test to prove detect a bug in de xml parser

--- tests/arc/xml.php
+++ tests/arc/xml.php
@@ -101,6 +101,18 @@
 			$this->assertTrue( $correct[0]->nodeValue == 'something' );			
 		}
 	
+		function testDataValue() {
+			$xml = arc\xml::parse('<?xml version="1.0" encoding="ISO-8859-1" ?>
+<rss version="2.0"><channel>
+<title>W3Schools Home Page</title><link>http://www.w3schools.com</link>
+<description>Free web building tutorials</description>
+<item><title>RSS Tutorial</title><link>http://www.w3schools.com/rss</link><description>New RSS tutorial on W3Schools</description></item>
+  <item><title>XML Tutorial</title><link>http://www.w3schools.com/xml</link><description>New XML tutorial on W3Schools</description></item>
+  </channel></rss> ');
+			$y = (int) $xml->el->nodeValue;
+			$this->assertTrue( is_int ($y) );
+		}
+
 		function testNamespaceCorrection() {
 			$xml = \arc\xml::parse( $this->namespacedXML );
 			$ns = $xml->testroot[0]->lookupNamespace('http://www.example.com/','bar');
@@ -115,5 +127,6 @@
 			$this->assertTrue( count( $allbars ) == 5 );
 			$this->assertTrue( $bars[1]->tagName == 'foo:bar' );
 		}
+
 		
 	}
