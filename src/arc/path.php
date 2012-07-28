cbebc7df047d3be2d6b0b8059ad195d4a7d0848c
Auke van Slooten <auke@poefke.nl>
2012-07-28 15:44:51 +0200

    - \arc\path::normalize => \arc\path::collapse - added caching of collapsed paths - factor 10 speedup - added \arc\path::walk method

--- src/arc/path.php
+++ src/arc/path.php
@@ -11,8 +11,23 @@
 
 	namespace arc;
 
+	/**
+		Utility methods to handle common path related tasks, cleaning, changing relative to absolute, etc.
+	*/
 	class path extends Pluggable {
+		
+		protected static $collapseCache = array();
 
+		/**
+			This method returns all the parent paths for a given path, starting at the root and including the given path itself.
+			
+			Usage:
+				\arc\path::parents( '/foo/bar/doh/', '/foo/' ); // => [ '/foo/', '/foo/bar/', '/foo/bar/doh/' ]
+
+			@param string $path The path to derive all parent paths from.
+			@param string $root The root or topmost parent to return. Defaults to '/'.
+			@returns array Array of all parent paths, starting at the root and ending with the given path. Note: It includes the given path!
+		*/
 		public static function parents( $path, $root = '/' ) {
 			// returns all parents starting at the root, up to and including the path itself
 			$prevpath = '/';
@@ -26,48 +41,70 @@
 			return $parents;
 		}
 
-		public static function normalize( $path, $cwd = '/' ) {
+		/**
+			This method parses a path which may contain '..' or '.' or '//' entries and returns the resulting absolute path.
+
+			Usage:
+				\arc\path::collapse( '../', '/foo/bar/' ); // => '/foo/'
+				\arc\path::collapse( '\\foo\\.\\bar/doh/../' ); // => '/foo/bar/'
+
+			@param string $path The input path, which may be relative. If this path starts with a '/' it is considered to start in the root.
+			@param string $cwd The current working directory. For relative paths this is the starting point.
+			@returns string The absolute path, without '..', '.' or '//' entries.
+		*/
+		public static function collapse( $path, $cwd = '/' ) {
 			// removes '.', changes '//' to '/', changes '\\' to '/', calculates '..' up to '/'
-			$path = str_replace('\\', '/', $path);
-			if ( is_object( $path ) && method_exists( $path, '__toString' ) ) {
-				$path = (string) $path;
+			if ( $path[0] !== '/' && $path[0] !== '\\' ) {
+				$path = $cwd . '/' . $path;
 			}
-			if ( !is_string( $path ) ) {
+			if ( isset( self::$collapseCache[$path] ) ) { // cache hit - so return that
+				return self::$collapseCache[$path];
+			}
+			$tempPath = str_replace('\\', '/', (string) $path);
+			if ( !$tempPath ) {
 				return $cwd;
 			}
-			if ( isset($path[0]) ) {
-				if ( $path[0] !== '/' ) {
-					$path = $cwd . '/' . $path;
-				}
-			}
-			if ( !$path ) {
-				return $cwd;
-			} else {
-				return '/' . self::reduce( $path, function( $result, $entry ) {
+			$collapsedPath = self::reduce( 
+				$tempPath, 
+				function( $result, $entry ) {
 					switch ( $entry ) {
 						case '..' :
 							$result = dirname( $result );
 							if ( isset($result[1]) ) { // fast check to see if there is a dirname
 								$result .= '/';
-							} else if ( $result === '.' ) { // dirname('foo') returns '.' so clear it
-								$result = '';
 							}
+							$result[0] = '/'; // php has a bug in dirname('/') -> returns a '\\' in windows
 						break;
 						case '.':
-						case '':
 						break;
 						default:
 							$result .= $entry .'/';
 						break;
 					}
 					return $result;
-				});
-			}
+				},
+				'/' // initial value, always start paths with a '/'
+			);
+			self::$collapseCache[$path] = $collapsedPath; // store the collapsed path in the cache, improves performance by factor > 10.
+			return $collapsedPath;
 		}
 
-		public static function clean( $path, $callback = null, $flags = null ) {
-			if ( !is_callable( $callback ) ) {
-				$filter = $callback;
+		/**
+			This method cleans the input path with the given filter method. You can specify any of the sanitize methods valid
+			for filter_var or you can use your own callback function. By default it url encodes each filename in the path.
+
+			Usage:
+				\arc\path::clean( '/a path/to somewhere/' ); // => '/a%20path/to%20somewhere/'
+
+			@param string $path The path to clean. 
+			@param mixed $filter Either one of the sanitize filters for filter_var or a callback method as in \arc\path::map
+			@param mixed $flags Optional list of flags for the sanitize filter.
+			@returns string The cleaned path.
+		*/
+		public static function clean( $path, $filter = null, $flags = null ) {
+			if ( is_callable( $filter ) ) {
+				$callback = $filter;
+			} else {
 				if ( !isset( $filter ) ) {
 					 $filter = FILTER_SANITIZE_ENCODED;
 				}
@@ -81,45 +118,103 @@
 			return self::map( $path, $callback );
 		}
 
+		/**
+			Returns either the immediate parent path for the given path, or null if it is outside the root path.
+			Differs with dirname() in that it will not return '/' as a parent of '/', but null instead.
+
+			Usage:
+				\arc\path::parent( '/foo/bar/' ); // => '/foo/'
+
+			@param string $path The path from which to get the parent path.
+			@param string $root Optional root path, defaults to '/'
+			@returns string|null The parent of the given path or null if the parent is outside the root path.
+		*/
 		public static function parent( $path, $root = '/' ) {
-			if ( $path == '/' ) {
+			if ( $path == $root ) {
 				return null;
 			}
 			$parent = dirname( $path );
 			if ( isset($parent[1]) ) { // fast check to see if there is a dirname
 				$parent .= '/';
 			}
-			if ( strpos( $parent, $root ) !== 0 ) {
+			if ( strpos( $parent, $root ) !== 0 ) { // parent is outside of the root
 				return null;
 			}
 			return $parent;
 		}
 
-		public static function map( $path, $callback ) {
-			$splitpath = array_filter( explode( '/', $path ), function( $entry ) {
+		protected static function getSplitPath( $path ) {
+			return array_filter( explode( '/', $path ), function( $entry ) {
 				return ( isset( $entry ) && $entry !== '' );
 			});
-			if ( count($splitpath) ) {
-				$result = array_map( $callback, $splitpath );
+		}
+
+		/**
+			Applies a callback function to each filename in a path. The result will be the new filename.
+
+			Usage:
+				/arc/path::map( '/foo>bar/', function( $filename ) { 
+					return htmlentities( $filename, ENT_QUOTES );
+				} ); // => '/foo&gt;bar/'
+
+			@param string $path The path to alter.
+			@param Callable $callback 
+			@returns string A path with all filenames changed as by the callback method.
+		*/
+		public static function map( $path, $callback ) {
+			$splitPath = self::getSplitPath( $path );
+			if ( count($splitPath) ) {
+				$result = array_map( $callback, $splitPath );
 				return '/' . join( $result, '/' ) .'/';
 			} else {
 				return '/';
 			}
 		}
 
+		/**
+			Applies a callback function to each filename in a path, but the result of the callback is fed back 
+			to the next call to the callback method as the first argument.
+			
+			Usage:
+				/arc/path::reduce( '/foo/bar/', function( $previousResult, $filename ) {
+					return $previousResult . $filename . '\\';
+				}, '\\' ); // => '\\foo\\bar\\'
+
+			@param string $path The path to reduce.
+			@param Callable $callback The method to apply to each filename of the path
+			@param mixed $initial Optional. The initial reduced value to start the callback with.
+		*/
 		public static function reduce( $path, $callback, $initial = null ) {
-			$splitpath = array_filter( explode( '/', $path ), function( $entry ) {
-				return ( isset( $entry ) && $entry !== '' );
-			});
-			return array_reduce( $splitpath, $callback, $initial );
+			return array_reduce( self::getSplitPath( $path ), $callback, $initial );
 		}
 
-		public static function collapseTree( $tree ) {
-			// TODO: collapse a tree structured array to a realized path / collapsed path array
-		}
+		/**
+			Applies a callback function to each parent of a path, in order. Starting at the root by default, but optionally
+			in reverse order. Will continue while the callback method returns null, otherwise returns the result of the callback method.
 
-		public static function expandTree( $array ) {
-			// TODO: inverse of collapseTree
+			Usage:
+				$result = \arc\path::walk( '/foo/bar/', function( $parent ) {
+					if ( $parent == '/foo/' ) { // silly test
+						return true;
+					}
+				});
+
+			@param string $path Each parent of this path will be passed to the callback method.
+			@param Callable $callback The method to apply to each parent
+			@param bool $startAtRoot Optional. If set to false, root will be called last, otherwise first. Defaults to true.
+			@param string $root Optional. Specify another root, no parents above the root will be called. Default to '/'.
+		*/
+		public static function walk( $path, $callback, $startAtRoot = true, $root = '/' ) {
+			$parents = self::parents( $path, $root );
+			if ( !$startAtRoot ) {
+				$parents = array_reverse( $parents );
+			}
+			foreach ( $parents as $parent ) {
+				$result = call_user_func( $callback, $parent );
+				if ( isset( $result ) ) {
+					return $result;
+				}
+			}
 		}
 
 	}
