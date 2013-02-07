110375f5ff716345c984b78c43ad4b37e8000fb9
poef <auke@muze.nl>
2013-02-07 15:55:22 +0100

    renamed parse to substitute, since parse is too generic renamed clean to substituteAll for the same reason added matched key as argument to a callable value updated docblocks

--- src/arc/template.php
+++ src/arc/template.php
@@ -5,15 +5,19 @@ namespace arc;
 class template {
 
 	/**
-	* This method replaces {{key}} entries in a string with the value of that key in an arguments array
+	* This method replaces {{key}} entries in a string with the value of that key in an arguments list
 	* If the key isn't in the arguments array, it will remain in the returned string as-is.
+	* The arguments list may be an object or an array and the values may be basic types or callable.
+	* In the latter case, the key will be substituted with the return value of the callable. The callable
+	* is called with the key matched.
 	* <code>
-	*   $parsedTemplate = \arc\template::parse( 'Hello {{world}}', [ 'world' => 'World!' ] );
+	*   $parsedTemplate = \arc\template::substitute( 'Hello {{world}} {{foo}}', [ 'world' => 'World!' ] );
+	*   // => 'Hello World! {{foo}}'
 	*</code>
 	* @param string $template
-	* @param array $arguments
+	* @param array $arguments 
 	*/
-	public static function parse( $template, $arguments ) {
+	public static function substitute( $template, $arguments ) {
 		if ( is_object($arguments) && !($arguments instanceof \ArrayObject ) ) {
 			$arguments = get_object_vars( $arguments );
 		}
@@ -21,13 +25,22 @@ class template {
 		return preg_replace_callback( '/'.$regex.'/', function( $matches ) use ( $arguments ) {
 			$argument = $arguments[ $matches[1] ];
 			if ( is_callable( $argument ) ) {
-				$argument = call_user_func( $argument );
+				$argument = call_user_func( $argument, $matches[1] );
 			}
 			return $argument;
 		}, $template );
 	}
 	
-	public static function clean( $template ) {
-		return preg_replace('/\{\{.*\}\}/', '', $template );
+	/**
+	* This method is identical to \arc\template::substitute but it removes any keys left over.
+	* <code>
+	*   $parsedTemplate = \arc\template::substituteAll( 'Hello {{world}} {{foo}}', [ 'world' => 'World!' ] );
+	*   // => 'Hello World! '
+	*</code>
+	* @param string $template
+	* @param array $arguments
+	*/
+	public static function substituteAll( $template, $arguments ) {
+		return preg_replace('/\{\{.*\}\}/', '', self::substitute( $template, $arguments ) );
 	}
 }
