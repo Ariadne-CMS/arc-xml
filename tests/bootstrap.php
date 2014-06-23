4dbdcfe7fa0871b265293e2c8f236dc48feaf04c
Robbert Müller <muller@muze.nl>
2014-06-23 14:12:47 +0200

    Assume the first bootstrap.php has an initialized vendor dir which is needed for testing from arc-arc

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
-	error_reporting(E_ALL|E_STRICT);
-	include_once( __DIR__ . '/../../../autoload.php'); // arc/arc autoloader
-//	require_once( __DIR__ . '/../autoload.php');
-//	require_once( __DIR__ . '/vendor/autoload.php');	
+	if ( ! defined ('SIMPLETESTDIR' ) ) {
+		define('SIMPLETESTDIR', __DIR__);
+	}
 
-	require_once( __DIR__ . '/vendor/lastcraft/simpletest/autorun.php');
-	require_once( __DIR__ . '/vendor/lastcraft/simpletest/compatibility.php');
-	require_once( __DIR__ . '/vendor/lastcraft/simpletest/browser.php');
-	require_once( __DIR__ . '/vendor/lastcraft/simpletest/web_tester.php');
-	require_once( __DIR__ . '/vendor/lastcraft/simpletest/unit_tester.php');
+	error_reporting(E_ALL|E_STRICT);
+	include_once( SIMPLETESTDIR . '/../vendor/autoload.php');
+
+	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/autorun.php');
+	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/compatibility.php');
+	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/browser.php');
+	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/web_tester.php');
+	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/unit_tester.php');
