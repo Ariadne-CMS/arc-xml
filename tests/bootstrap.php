b5b249bf25651b46169d299c2561b0892d90674f
Robbert Müller <spam.me@grols.ch>
2014-07-05 08:34:30 +0200

    Convert simpletest to phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,9 @@
 <?php
 
-	if ( ! defined ('SIMPLETESTDIR' ) ) {
-		define('SIMPLETESTDIR', __DIR__);
-	}
+    if ( ! defined ('TESTDIR' ) ) {
+        define('TESTDIR', __DIR__);
+    }
 
-	error_reporting(E_ALL|E_STRICT);
-	include_once( SIMPLETESTDIR . '/../vendor/autoload.php');
+    error_reporting(E_ALL|E_STRICT);
+    include_once( TESTDIR . '/../vendor/autoload.php');
 
-	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/autorun.php');
-	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/compatibility.php');
-	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/browser.php');
-	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/web_tester.php');
-	require_once( SIMPLETESTDIR . '/vendor/lastcraft/simpletest/unit_tester.php');
