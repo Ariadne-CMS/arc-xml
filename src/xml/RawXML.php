6ce67f92d4cea6c344e04bfeb10a49ceb102a313
Auke van Slooten <auke@muze.nl>
2016-01-25 13:46:03 +0100

    added missing class

---
+++ src/xml/RawXML.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Ariadne Component Library.
+ *
+ * (c) Muze <info@muze.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace arc\xml;
+
+/**
+ * This is a container for raw xml content, which will not get escaped 
+ * when included by the \arc\xml\Writer or NodeList.
+ * @property string $contents
+ */
+class RawXML {
+    public $contents = '';
+
+    public function __construct($contents) 
+    {
+        $this->contents = $contents;
+    }
+
+    public function __toString()
+    {
+        return $this->contents;
+    }
+}
